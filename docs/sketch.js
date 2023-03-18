58f4e9d90bd96d27b5d70dbd8bbb7734025fbaae
Ldmoralesv <126963374+Ldmoralesv@users.noreply.github.com>
2023-03-18 10:52:56 -0500

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,48 +1,100 @@
-// Where is the car
-let x, y;
+let pinky, msduapend;
+let pinkyX, msduapendX;
+let pinkyY, msduapendY;
+let speed = 4;
+let jumping = false;
+let jumpCounter = 0;
 
 class Carro {
   constructor(alto, ancho, color, name) {
     this.alto = alto;
     this.ancho = ancho;
     this.color = color;
-    this.name = name
-    }
+    this.name = name;
   }
 
-function setup() {
-  createCanvas(500, 300);
-  // Starts in the middle
-  y = height - height/4;
-  x = 0;
-  let miCarro = new Carro (10, 20, red, "ms duapend"); // creating new instance of Polygon Class.
-  console.log(miCarro.alto); // prints '1' to the console
+  display(x, y) {
+    stroke(220, 200, 200);
+    fill(this.color);
+    rect(x, y, this.ancho, this.alto);
+  }
+}
 
+function setup() {
+  createCanvas(600, 500);
+  pinkyX = 0;
+  msduapendX = width;
+  pinkyY = height - height / 4;
+  msduapendY = height - height / 4;
+
+  pinky = new Carro(30, 60, "blue", "pinky");
+  msduapend = new Carro(30, 60, "red", "msduapend");
+
+  console.log("pinky ancho es " + pinky.ancho);
+  console.log("pinky alto es " + pinky.alto);
+  console.log("pinky color es " + pinky.color);
+  console.log("pinky name es " + pinky.name);
+
+  console.log("msduapend ancho es " + msduapend.ancho);
+  console.log("msduapend alto es " + msduapend.alto);
+  console.log("msduapend color es " + msduapend.color);
+  console.log("msduapend name es " + msduapend.name);
+} if (msduapendX > width - msduapend.ancho) {
+    msduapendX = width - msduapend.ancho;
+    speed *= -1;
+} else if (msduapendX < msduapend.ancho) {
+    msduapendX = msduapend.ancho;
+    speed *= -1;
 }
 
 function draw() {
-  background(100);  
-  display();
-  move();
+  background(20, 200, 150);
+
+  pinkyX += speed;
+  msduapendX -= speed;
+
+  if (pinkyX > width - pinky.ancho) {
+    pinkyX = width - pinky.ancho;
+    speed * -1;
+  } else if (pinkyX < 0) {
+    pinkyX = 0;
+    speed * -1;
+  }
+
+  if (msduapendX < msduapend.ancho) {
+    msduapendX = msduapend.ancho;
+    speed *= -1;
+  } else if (msduapendX > width) {
+    msduapendX = width;
+    speed *= -1;
+  }
+
+  if (jumping) {
+    if (jumpCounter < 10) {
+      pinkyY -= 5;
+    } else if (jumpCounter < 20) {
+      pinkyY += 5;
+    } else {
+      jumping = false;
+    }
+    jumpCounter++;
+  } else {
+    pinkyY = height - height / 4;
+  }
+
+  pinky.display(pinkyX, pinkyY);
+  msduapend.display(msduapendX, msduapendY);
 }
 
-function move(){
-  // Jiggling randomly
-  y = y + random(-1, 1);
-  
-  if (x > width) {
-    // Reset 
-    x = 0;
-  } else {
-    y = height - height/4;
-    // Moving up at a constant speed
-    x = x + 2;
+function salto() {
+  if (!jumping) {
+    jumping = true;
+    jumpCounter = 0;
   }
 }
 
-function display(){
-  // Draw a car
-  stroke(50);
-  fill(255,4,60);
-  rect(x, y, 80, 20);
+function keyPressed() {
+  if (keyCode === 32) {
+    salto();
+  }
 }
